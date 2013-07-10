0e3a749fe5069c4d32f320b6c211c03207684410
fand <s0811489@coins.tsukuba.ac.jp>
2013-07-11 02:01:10 +0900

    js-3 passed

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,15 @@
 // 課題 JS-3 の実装をここに記述してください。
+$("submit-button").addEventListener("click", function(){
+    var log = $("log-input").value;
+    var dst = $("table-container");
+
+    // make sure there is only 1 table.
+    if (dst.innerHTML == "") {
+        createLogTable(dst, parseLTSVLog(log));
+    }
+});
+
+// getElementById
+function $(id){
+    return document.getElementById(id);
+}
